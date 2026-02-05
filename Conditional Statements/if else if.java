bd3adbe1e9a212a4e6ca0b76bee7cc1e22432ff6
SENODROOM <136210526+SENODROOM@users.noreply.github.com>
2026-02-06 04:23:58 +0500

    Conditional Statements

---
+++ Conditional Statements/if else if.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 0;
+
+        if (number > 0) {
+            System.out.println("Positive number");
+        } else if (number < 0) {
+            System.out.println("Negative number");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
